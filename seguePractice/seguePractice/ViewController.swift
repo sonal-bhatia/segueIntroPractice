c3a747e5b0ee02756d47c0303b81e1ebb9b92663
sonal-bhatia <69254430+sonal-bhatia@users.noreply.github.com>
2020-08-06 12:00:06 -0400

    Add files via upload

---
+++ seguePractice/seguePractice/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  seguePractice
+//
+//  Created by Sonal Bhatia on 8/6/20.
+//  Copyright © 2020 Sonal Bhatia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
